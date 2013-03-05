3fbab4c64b0d2572155bd3e677d413469e52d4b5
wlzch <wilzichi92@gmail.com>
2013-03-06 00:39:36 +0700

    Ubah tampilan exercise.

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-  $('pre.htmlCode').snippet('html', {style: 'vim-dark', clipboard: '/js/ZeroClipboard.swf'});
-  $('pre.cssCode').snippet('css', {style: 'neon', clipboard: '/js/ZeroClipboard.swf'});
+  $('pre.htmlCode').snippet('html', {style: 'vim', clipboard: '/js/ZeroClipboard.swf'});
+  $('pre.cssCode').snippet('css', {style: 'vim', clipboard: '/js/ZeroClipboard.swf'});
 });
