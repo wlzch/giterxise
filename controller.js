ab72c39fc8dafc5fcd6a1a87c52fa2f15258fb7f
wlzch <wilzichi92@gmail.com>
2013-03-05 09:54:43 +0700

    Include exercises dynamically.

--- controller.js
+++ controller.js
@@ -1,11 +1,20 @@
-var controller = {};
-var yaml = require('js-yaml');
-
-var config = require('./config.yml');
+var
+  yaml = require('js-yaml'),
+  _ = require('underscore'),
+  controller = {}
+;
 
 controller.index = function(req, res) {
+  var config = require('./config.yml');
+  var exercises = {};
+  _.each(config.exercises, function(exercise) {
+    res.render('exercises/exercise'+exercise, function(err, html) {
+      exercises['tab'+exercise] = html;
+    });
+  });
   res.render('index', {
-    succeeds: config.succeeds
+    exerciseNumbers: config.exercises,
+    exercises: exercises
   });
 };
 
